c880971027705e3747406c17d9ba44dea26539ef
Bright_Peerapat <Peerapat@example.com>
2023-11-06 01:40:27 +0700

    Initial Commit

---
+++ SlideNumber/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SlideNumber
+//
+//  Created by Mac-Peerapat on 6/11/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SlideNumber/SlideNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideNumberApp.swift
+//  SlideNumber
+//
+//  Created by Mac-Peerapat on 6/11/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct SlideNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
